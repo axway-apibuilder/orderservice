9e7f5256b30cc8a466c135089ad7e9c75550adcc
Rathna <rnatarajan@axway.com>
2020-10-04 00:14:41 -0700

    env mysql cred

--- conf/mysql.default.js
+++ conf/mysql.default.js
@@ -16,10 +16,8 @@ module.exports = {
 			host: 'localhost',
 			port: 3306,
 			database: 'orders',
-			//user: process.env.MYSQL_USER,
-			//password: process.env.MYSQL_PASSWORD,
-          	user: 'root',
-          	password: 'axway',
+			user: process.env.MYSQL_USER,
+			password: process.env.MYSQL_PASSWORD,
 
 			// Create models based on your schema that can be used in your API.
 			generateModelsFromSchema: true,
